d9e7ed9a3fd3a0f0026646793d6a109ebe818f71
Merwin-Rebello <merwinrebello02@gmail.com>
2024-06-10 16:59:05 +0530

    profile, discover, pages rightsidebar and  more completed

--- components/Leftsidebar.tsx
+++ components/Leftsidebar.tsx
@@ -1,15 +1,27 @@
 "use client";
-import React from "react";
-import Link from "next/link";
-import Image from "next/image";
+
 import { sidebarLinks } from "@/constants";
-import { usePathname, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
-const Leftsidebar = () => {
+import { SignedIn, SignedOut, useClerk } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+import { Button } from "./ui/button";
+import { useAudio } from "@/provider/AudioProvider";
+
+const LeftSidebar = () => {
   const pathname = usePathname();
   const router = useRouter();
+  const { signOut } = useClerk();
+  const { audio } = useAudio();
+
   return (
-    <section className="left_sidebar">
+    <section
+      className={cn("left_sidebar h-[calc(100vh-5px)]", {
+        "h-[calc(100vh-140px)]": audio?.audioUrl,
+      })}
+    >
       <nav className="flex flex-col gap-6">
         <Link
           href="/"
@@ -20,27 +32,47 @@ const Leftsidebar = () => {
             Podcaster
           </h1>
         </Link>
+
         {sidebarLinks.map(({ route, label, imgURL }) => {
           const isActive =
-            pathname === route || pathname.startsWith(`${route}/`); //to check which route is currenlty active to give it special css
+            pathname === route || pathname.startsWith(`${route}/`);
+
           return (
             <Link
               href={route}
               key={label}
               className={cn(
-                "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start ",
-                { "bg-nav-focus border-r-4 border-orange-1": isActive }
+                "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
+                {
+                  "bg-nav-focus border-r-4 border-orange-1": isActive,
+                }
               )}
             >
               <Image src={imgURL} alt={label} width={24} height={24} />
-
               <p>{label}</p>
             </Link>
           );
         })}
       </nav>
+      <SignedOut>
+        <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
+          <Button asChild className="text-16 w-full bg-orange-1 font-extrabold">
+            <Link href="/sign-in">Sign in</Link>
+          </Button>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
+          <Button
+            className="text-16 w-full bg-orange-1 font-extrabold"
+            onClick={() => signOut(() => router.push("/"))}
+          >
+            Log Out
+          </Button>
+        </div>
+      </SignedIn>
     </section>
   );
 };
 
-export default Leftsidebar;
+export default LeftSidebar;
